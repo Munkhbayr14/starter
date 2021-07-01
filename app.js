6a03d6e2c9f1d859a29319eb9452fe53aeeb925c
Munkhbayr14 <monhbayrm14@gmail.com>
2021-07-01 20:08:19 +0800

    орлого зарлагаа хадгалах код бичнэ

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // дэлгэцтэй ажиллах контроллор
 var uiController = (function () {
+  // html css-ийн class болон ID-ийг нэг object-д хийсэн
+  // private data
   var DOMstring = {
     inputType: ".add__type",
     inputDescription: ".add__description",
     inputValue: ".add__value",
     addBtn: ".add__btn",
   };
+  // public function
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstring.inputType).value,
-        description: document.querySelector(DOMstring.inputDescription).value,
-        value: document.querySelector(DOMstring.inputValue).value,
+        type: document.querySelector(DOMstring.inputType).value, // income or expense
+        description: document.querySelector(DOMstring.inputDescription).value, // description тайлбар авах хэсэг
+        value: document.querySelector(DOMstring.inputValue).value, // value буюу мөнгөн дүнг авах
       };
     },
+    //html css-ийн class болон ID-ыг public type-тай буцааж байна
     getDOMstring: function () {
       return DOMstring;
     },
@@ -32,8 +36,9 @@ var financeController = (function () {
     this.description = description;
     this.value = value;
   };
+  // орлого зарлагийг нэг объектэд хадаглаж байна.
   var data = {
-    allItem: {
+    Item: {
       inc: [],
       exp: [],
     },
@@ -42,18 +47,40 @@ var financeController = (function () {
       exp: 0,
     },
   };
+  return {
+    // public data
+    addItem: function (type, desc, val) {
+      var item, id;
+      // массив доторх урт нь 0 байвал id-ийн 1 ээр өгч байгаа
+      if (data.Item[type].length === 0) id = 1;
+      // массив дотороос элетэнт устагаж байгаа тохиолдолд массивийн уртас хасанж ID-г 1 ээр нэмнэ.
+      else {
+        id = data.Item[type][data.Item[type].length - 1].id + 1;
+      }
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        // type === exp
+        item = new Expense(id, desc, val);
+      }
+      data.Item[type].push(item);
+    },
+  };
 })();
 
 // прогараммыг холбогч контроллор
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
-    console.log(uiController.getInput());
     //1. textBox хэсгээс хэрэглэгчийн оруулсан өгөгдлийг олж авна.
-    //2. олж авсан өгөгдлүүдээ санхүүгийн контролд дамжуулж тэнд хадгална.
+    var input = uiController.getInput();
+    //2.0 олж авсан өгөгдлүүдээ санхүүгийн контролд дамжуулж тэнд хадгална.
+    //2.1 input дотор манай гараас авсан утгууд байгаа. тэрийгээ санхүүгийн [addItem] руу хийж байгаа
+    financeController.addItem(input.type, input.description, input.value);
     //3. олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэгт харуулна.
     //4. Төсвийн тооцоолол хийнэ
     //5.Эцсийн үлдэгдэл тооцоог дэлгэцэнд харуулна.
   };
+  // бүх event-ийг нэг функц дотор хийсэн
   var setUpEventListener = function () {
     var DOM = uiController.getDOMstring();
     document.querySelector(DOM.addBtn).addEventListener("click", function () {
@@ -66,6 +93,7 @@ var appController = (function (uiController, financeController) {
       }
     });
   };
+  // setUpEventListener функц маань private төрөлтэй байгаа учир түүнийг [public] төрөлтэй appcontroller- руу буцааж байна
   return {
     init: function () {
       console.log("application started...");
